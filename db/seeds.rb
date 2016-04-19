b91bcf51a33dd808e8abdc4ef5946f266af2df6b
Dennis B <dennisb@example.com>
2016-04-19 16:03:55 +1000

    Initial Artists seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Raw Artist list.
+#  % for i in $(find . -name '01*.mp3'); do mediainfo $i | grep "^Performer"; done | sort | uniq
+
+seeds_file = Rails.root.join("db", "seeds", "seeds.yml")
+config = YAML::load_file(seeds_file)
+config["artists"].each do |values|
+  Artist.find_or_create_by(values)
+end
