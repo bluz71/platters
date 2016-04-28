ad7ca6e6f4098555e87e2fcd5b7076dcdec8c034
Dennis B <dennisb@example.com>
2016-04-28 16:56:24 +1000

    Fleshed out Artists and Albums YML lists.
    Much more to-do still.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 #  % for i in $(find . -name '01*.mp3'); do mediainfo $i | grep "^Performer"; done | sort | uniq
 #
 # Raw Album list:
-#  % for i in $(find . -name '01*.mp3' | sort); do mediainfo $i | grep "^Performer\|^Album " | sort -r ;echo; done
+#  % for i in $(find . -name '01*.mp3' | sort); do mediainfo $i | grep "^Performer\|^Album \|^Recorded\|^Genre";echo; done > albums.yml
 
 artists_seeds = Rails.root.join("db", "seeds", "artists.yml")
 artists = YAML::load_file(artists_seeds)
@@ -15,5 +15,7 @@ albums = YAML::load_file(albums_seeds)
 albums.each do |album|
   artist = Artist.find_by(name: album["artist"])
   album.delete("artist")
+  album.delete("genre")
+  album.delete("year")
   artist.albums.find_or_create_by(album)
 end
