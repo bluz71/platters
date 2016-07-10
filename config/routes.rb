fa569a654a101a309924aaf2cf6d7abc664d1ef6
Dennis B <dennisb@example.com>
2016-07-10 18:10:41 +1000

    Removed controller names from a few URLs.
    For example album show will now be:
    
      host.com/ac_dc/back_in_black
    
    instead of:
    
      host.com/artists/ac_dc/albums/back_in_black

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   root "artists#index"
 
-  resources :artists do
-    resources :albums, except: [:index]
-  end
+  # Nested resources, Artist and Album, both using FriendlyId with blank
+  # controller names.
+  # 
+  # Important, read the following, especially the "small problem" section:
+  #   http://jasoncodes.com/posts/rails-3-nested-resource-slugs
 
+  resources :artists, only: [:index, :new, :create]
   resources :albums, only: [:index]
+
+  resources :artists, path: "", except: [:index, :new, :create]
+
+  resources :artists, path: "", only: [] do
+    resources :albums, path: "", except: [:index]
+  end
 end
