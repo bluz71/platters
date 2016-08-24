3e5274ba1f2b75205b08a291a05fb6472fec97fc
Dennis B <dennisb@example.com>
2016-08-24 16:34:47 +1000

    Make these JS tests more reliable by rerunning them if needed.
    For reasons not obvious to me these JS tests are flaky, GET parameters are not
    always set as expected (could be a PhantomJS issue).
    
    Rerunning the test via internal loops appears to work well. Stick a hack
    though.

--- spec/features/filtering_albums_spec.rb
+++ spec/features/filtering_albums_spec.rb
@@ -21,130 +21,187 @@ RSpec.feature "Filtering albums" do
                        release_date: release_date3)
   end
 
-  before do
-    visit albums_path
-    page.find(".filter-link").click
-    wait_for_js
-  end
-
   context "by genre" do
     scenario "with default title sorting", js: true do
-      select "Rock", from: "Genre" 
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit albums_path(genre: "Rock") if URI.parse(current_url).request_uri != albums_path(genre: "Rock")
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        select "Rock", from: "Genre" 
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == albums_path(genre: "Rock")
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("First")
       expect(albums[1]).to have_content("Second")
+      expect(page).to have_current_path(albums_path(genre: "Rock"))
     end
 
     scenario "with reversed title sorting", js: true do
-      select "Rock", from: "Genre" 
-      choose "Reverse"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit albums_path(genre: "Rock", order: "reverse") if URI.parse(current_url).request_uri != albums_path(genre: "Rock", order: "reverse")
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        select "Rock", from: "Genre" 
+        choose "Reverse"
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == albums_path(genre: "Rock", order: "reverse")
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("Second")
       expect(albums[1]).to have_content("First")
+      expect(page).to have_current_path(albums_path(genre: "Rock", order: "reverse"))
     end
 
     scenario "with year sorting", js: true do
-      select "Rock", from: "Genre" 
-      choose "Year"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit albums_path(genre: "Rock", sort: "year") if URI.parse(current_url).request_uri != albums_path(genre: "Rock", sort: "year")
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        select "Rock", from: "Genre" 
+        choose "Year"
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == albums_path(genre: "Rock", sort: "year")
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("First")
       expect(albums[1]).to have_content("Second")
+      expect(page).to have_current_path(albums_path(genre: "Rock", sort: "year"))
     end
 
     scenario "with year sorting reversed", js: true do
-      select "Rock", from: "Genre" 
-      choose "Year"
-      choose "Reverse"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit "/albums?genre=Rock&sort=year&order=reverse" if URI.parse(current_url).request_uri != "/albums?genre=Rock&sort=year&order=reverse"
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        select "Rock", from: "Genre" 
+        choose "Year"
+        choose "Reverse"
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == "/albums?genre=Rock&sort=year&order=reverse"
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("Second")
       expect(albums[1]).to have_content("First")
+      expect(page).to have_current_path("/albums?genre=Rock&sort=year&order=reverse")
     end
   end
 
   context "by year" do
     scenario "with default title sorting", js: true do
-      fill_in "year", with: "2000, 2005"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit albums_path(year: "2000, 2005") if URI.parse(current_url).request_uri != albums_path(year: "2000, 2005")
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        fill_in "year", with: "2000, 2005"
+        click_on "Select"
+        wait_for_js
+        break if  URI.parse(current_url).request_uri == albums_path(year: "2000, 2005")
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("First")
       expect(albums[1]).to have_content("Second")
+      expect(page).to have_current_path(albums_path(year: "2000, 2005"))
     end
 
     scenario "with reversed title sorting", js: true do
-      fill_in "year", with: "2000, 2005"
-      choose "Reverse"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit "/albums?year=2000%2C+2005&order=reverse" if URI.parse(current_url).request_uri != "/albums?year=2000%2C+2005&order=reverse"
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        fill_in "year", with: "2000, 2005"
+        choose "Reverse"
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == "/albums?year=2000%2C+2005&order=reverse"
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("Second")
       expect(albums[1]).to have_content("First")
+      expect(page).to have_current_path("/albums?year=2000%2C+2005&order=reverse")
     end
 
     scenario "with range", js: true do
-      fill_in "year", with: "2000..2010"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit albums_path(year: "2000..2010") if URI.parse(current_url).request_uri != albums_path(year: "2000..2010")
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      5.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        fill_in "year", with: "2000..2010"
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == albums_path(year: "2000..2010")
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 3
       expect(albums[0]).to have_content("First")
       expect(albums[1]).to have_content("Second")
       expect(albums[2]).to have_content("Third")
+      expect(page).to have_current_path(albums_path(year: "2000..2010"))
     end
 
     scenario "with genre", js: true do
-      fill_in "year", with: "2000, 2005"
-      select "Rock", from: "Genre" 
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit albums_path(genre: "Rock", year: "2000, 2005") if URI.parse(current_url).request_uri != albums_path(genre: "Rock", year: "2000, 2005")
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      15.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        fill_in "year", with: "2000, 2005"
+        select "Rock", from: "Genre" 
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == albums_path(genre: "Rock", year: "2000, 2005")
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("First")
       expect(albums[1]).to have_content("Second")
+      expect(page).to have_current_path(albums_path(genre: "Rock", year: "2000, 2005"))
     end
 
     scenario "with genre reversed", js: true do
-      fill_in "year", with: "2000, 2005"
-      select "Rock", from: "Genre" 
-      choose "Reverse"
-      click_on "Select"
-      # Sometimes expected GET parameters go missing from this JS test, hence
-      # must go visit filter URL directly.
-      visit "/albums?genre=Rock&year=2000%2C+2005&order=reverse" if URI.parse(current_url).request_uri != "/albums?genre=Rock&year=2000%2C+2005&order=reverse"
+      # Sometimes expected GET parameters mysteriously go missing from this JS
+      # test, hence try a few times.
+      15.times do
+        visit albums_path
+        page.find(".filter-link").click
+        wait_for_js
+        fill_in "year", with: "2000, 2005"
+        select "Rock", from: "Genre" 
+        choose "Reverse"
+        click_on "Select"
+        wait_for_js
+        break if URI.parse(current_url).request_uri == "/albums?genre=Rock&year=2000%2C+2005&order=reverse"
+      end
       albums = page.all(".album")
       expect(albums.size).to eq 2
       expect(albums[0]).to have_content("Second")
       expect(albums[1]).to have_content("First")
+      expect(page).to have_current_path("/albums?genre=Rock&year=2000%2C+2005&order=reverse")
     end
   end
 end
