9445fdc777c1db88fd6272fb68343aec069be388
Dennis B <dennisb@example.com>
2016-09-28 15:02:41 +1000

    Completed Rackspace research and testing.
    For CarrierWave setting the Rackspace config.asset_host is a MUST. With it
    performance is good, without it performance is terrible.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,6 +20,13 @@ CarrierWave.configure do |config|
       rackspace_region:   :syd
     }
     config.fog_directory = ENV["RACKSPACE_CONTAINER"]
+
+    # CDN host name for this Rackspace container. This is optional but it SHOULD
+    # be set otherwise Cloud Files performance will be extremely poor.
+    #
+    # To find the asset host, log into the Rackspace web management interface
+    # and navigate to the Containers section and click on the cog symbol of the
+    # appropriate container, copy the HTTPS link to RACKSPACE_ASSET_HOST.
     config.asset_host =    ENV["RACKSPACE_ASSET_HOST"]
   elsif Rails.env.test?
     config.storage = :file
