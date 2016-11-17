9cd3f222d3b2ef3058c74020f93684ab3091bdf3
Dennis B <dennisb@example.com>
2016-11-17 13:17:48 +1100

    Moved comment rendering into a partial, plus added in comment caching.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   # ASSOCIATIONS
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
   belongs_to :user
 end
