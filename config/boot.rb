88aab1da5fdfa7aadf4cc05935c1c9fe1ed0110d
Dennis B <bluz71@users.noreply.github.com>
2017-09-11 16:15:37 +1000

    Trying Bootsnap again.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap'
+Bootsnap.setup(
+  cache_dir:  'tmp/cache',
+  development_mode: ENV['RAILS_ENV'] == 'development',
+  load_path_cache: true,
+  autoload_paths_cache: true,
+  disable_trace: false,
+  compile_cache_iseq: true,
+  compile_cache_yaml: true
+)
