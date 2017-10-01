e7192d82f81941b8d41fb485a2b11b744656c2e8
Dennis B <bluz71@users.noreply.github.com>
2017-10-01 16:25:27 +1100

    Disable Bootsnap load_path_cache and autoload_paths_cache.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup'
+
+require 'bootsnap'
+env = ENV['RAILS_ENV'] || "development"
+Bootsnap.setup(
+  cache_dir:            'tmp/cache',
+  development_mode:     env == 'development',
+  load_path_cache:      false,
+  autoload_paths_cache: false,
+  disable_trace:        true,
+  compile_cache_iseq:   true,
+  compile_cache_yaml:   true
+)
