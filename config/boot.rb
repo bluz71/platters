c68b7f383c14b0270ad1540c8e8e2ef50d33b5ef
Dennis B <bluz71@users.noreply.github.com>
2017-10-08 15:37:21 +1100

    Upgraded bootsnap gem.

--- config/boot.rb
+++ config/boot.rb
@@ -2,14 +2,15 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-require 'bootsnap'
-env = ENV['RAILS_ENV'] || "development"
-Bootsnap.setup(
-  cache_dir:            'tmp/cache',
-  development_mode:     env == 'development',
-  load_path_cache:      false,
-  autoload_paths_cache: false,
-  disable_trace:        true,
-  compile_cache_iseq:   true,
-  compile_cache_yaml:   true
-)
+require 'bootsnap/setup'
+# require 'bootsnap'
+# env = ENV['RAILS_ENV'] || "development"
+# Bootsnap.setup(
+#   cache_dir:            'tmp/cache',
+#   development_mode:     env == 'development',
+#   load_path_cache:      false,
+#   autoload_paths_cache: false,
+#   disable_trace:        true,
+#   compile_cache_iseq:   true,
+#   compile_cache_yaml:   true
+# )
