0a93cd7c9f591350171f03818d5178bb28819541
Dennis B <bluz71@users.noreply.github.com>
2018-04-11 14:37:06 +1000

    Fleshed out Home page endpoint.

--- app/views/misc_pages/home.json.jbuilder
+++ app/views/misc_pages/home.json.jbuilder
@@ -11,11 +11,14 @@ end
 json.most_recent do
   json.albums do
     json.array! Album.including.most_recent.each do |album|
-      json.extract! album, :id, :title
+      json.extract! album, :id, :title, :tracks_count, :comments_count
       json.artist album.artist.name
+      json.year album.release_date.year
+      json.genre album.genre.name
       json.artist_slug album.artist.slug
       json.album_slug album.slug
       json.cover_url album.cover.small.url
+      json.tracks album.tracks_summary
     end
   end
   json.comments do
---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+# Notes about API specs:
+#   http://matthewlehner.net/rails-api-testing-guidelines
+
+RSpec.describe "Home API" do
+  let(:genre)        { FactoryBot.create(:genre, name: "Rock") }
+  let(:artist)       { FactoryBot.create(:artist, name: "Artist") }
+  let(:release_date) { FactoryBot.create(:release_date, year: Date.current.year) }
+  let(:album)        { FactoryBot.create(:album, title: "ABC", artist: artist,
+                                         release_date: release_date) }
+
+  before do
+    10.times do
+      FactoryBot.create(:album, artist: artist, release_date: release_date)
+    end
+    12.times do
+      FactoryBot.create(:comment_for_artist, commentable: artist, body: "Comment")
+    end
+  end
+
+  scenario "provides home page resources" do
+    expect(Album).to receive(:spotlight) { album }
+    get "/home.json"
+
+    expect(response).to be_success
+    expect(json_response["album_of_the_day"]["title"]).to eq "ABC"
+    expect(json_response["most_recent"]["albums"].length).to eq 6
+    expect(json_response["most_recent"]["comments"].length).to eq 10
+  end
+end
