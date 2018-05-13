17091899e28549b027403b543d1a064d61c18ae5
Dennis B <bluz71@users.noreply.github.com>
2018-05-13 15:01:42 +1000

    API request spec for user comments.

---
+++ spec/requests/user_comments_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+# Notes about API specs:
+#   http://matthewlehner.net/rails-api-testing-guidelines
+
+RSpec.describe "User Comments API" do
+  let(:user)  { FactoryBot.create(:user) }
+  let(:artist) { FactoryBot.create(:artist) }
+
+  before do
+    FactoryBot.create(:comment_for_artist, commentable: artist, user: user,
+                      body: "First comment")
+    28.times do
+      FactoryBot.create(:comment_for_artist, commentable: artist, user: user,
+                        body: "A comment")
+    end
+    FactoryBot.create(:comment_for_artist, commentable: artist, user: user,
+                      body: "Newest comment")
+  end
+
+  scenario "provides a list of user comments" do
+    get "/comments/#{user.slug}.json"
+
+    expect(response).to be_successful
+    expect(json_response["comments"].length).to eq 25
+    expect(json_response["comments"][0]["body"]).to eq "Newest comment"
+  end
+
+  scenario "provides a list of paginated comments" do
+    get "/comments/#{user.slug}.json?page=2"
+
+    expect(response).to be_successful
+    expect(json_response["comments"].length).to eq 5
+    expect(json_response["comments"][4]["body"]).to eq "First comment"
+  end
+end
