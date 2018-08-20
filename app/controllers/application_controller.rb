12f16b75ac1f6b3a6ce89e9d2019c9a78760d1ae
Dennis B <bluz71@users.noreply.github.com>
2018-08-20 15:13:37 +1000

    Initial infrastructure to handle dual-authentication systems.
    API access will use new JWT logic, this will be separate to
    the existing session/cookie system done via the Clearance Gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
+  # Needed to handle dual-authentication: cookie-based for HTML access using
+  # the Clearance Gem and JWT-based for API access using custom application
+  # code.
+  alias_method :clearance_current_user, :current_user
+  alias_method :clearance_signed_in?, :signed_in?
+
+  protected
+
+    def current_user
+      clearance_current_user
+    end
+
+    def signed_in?
+      clearance_signed_in?
+    end
+
   private
 
     def require_admin
