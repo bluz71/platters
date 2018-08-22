4e7e782d9afe5cf3304bf7036e68a1ee5c5003f1
Dennis B <bluz71@users.noreply.github.com>
2018-08-22 13:09:46 +1000

    Fixes for API log_in end-point.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,8 +4,8 @@ class Api::SessionsController < ApplicationController
   before_action :set_user
 
   def create
-    if @user.authenticate(auth_params[:password])
-      token = ApiAuth.encode(user: user.id)
+    if @user.authenticated?(auth_params[:password])
+      token = ApiAuth.encode(user: @user.id, admin: @user.admin?)
       render json: {jwt: token}
     else
       head :unauthorized
@@ -16,7 +16,8 @@ class Api::SessionsController < ApplicationController
 
     def set_user
       @user = User.find_by(email: auth_params[:email])
-      head :not_found unless user.present? && user.email_confirmed_at.present?
+      head :not_found unless @user.present?
+      head :forbidden unless @user.email_confirmed_at.present?
     end
 
     def auth_params
