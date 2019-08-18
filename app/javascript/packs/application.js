8e258e14abb98d34e8d7293b2bd8dccce7c04f47
Dennis B <bluz71@users.noreply.github.com>
2019-08-18 16:17:43 +1000

    Moved images from asset pipeline to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,3 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
 import AlbumForm from '../src/AlbumForm'
 import AlbumTracksVisibility from '../src/AlbumTracksVisibility'
 import AlbumsSelect from '../src/AlbumsSelect'
@@ -28,6 +12,10 @@ require('bootstrap-sass/assets/javascripts/bootstrap')
 require('@rails/ujs').start()
 require('turbolinks').start()
 require('local-time').start()
+
+// Load images.
+require.context('../images', true, /\.(?:png|jpg|gif|ico|svg)$/)
+
 
 // Event handlers to run once the DOM is ready.
 $(() => {
@@ -58,14 +46,12 @@ $(document).on('turbolinks:load', () => {
       $(this).remove()
     })
 
-  // Instantiate a comments handler if needed.
+  // Instantiate a comments handler if one does not exist.
   if (!commentsShowMore) {
     commentsShowMore = new CommentsShowMore()
   }
-
   // End previous comments handling, we have moved to a new page.
   commentsShowMore.endCommentsHandling()
-
   // Start comments scroll handling for this new page.
   commentsShowMore.startCommentsHandling()
 })
