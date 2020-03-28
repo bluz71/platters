6a50855d7cbcbe2511e9b15ca83603918683ecb3
Dennis B <bluz71@users.noreply.github.com>
2020-03-28 15:08:52 +1100

    Fix scripts

--- db/add_comments.rb
+++ db/add_comments.rb
@@ -1,13 +1,13 @@
 artist = Artist.find(1234)
 rand(50).times do
-  comment = artist.comments.create(user: User.find(rand(3..52)),
+  comment = artist.comments.create(user: User.find(rand(3..51)),
                                    body: Faker::Hipster.paragraph[0..280])
   comment.update_attribute(:created_at, (rand + rand(300)).days.ago)
 end
 
 album = Album.find(1234)
 rand(50).times do
-  comment = album.comments.create(user: User.find(rand(3..52)),
+  comment = album.comments.create(user: User.find(rand(3..51)),
                                   body: Faker::Hipster.paragraph[0..280])
   comment.update_attribute(:created_at, (rand + rand(300)).days.ago)
 end
--- db/random_comments.rb
+++ db/random_comments.rb
@@ -1,5 +1,5 @@
 20.times do
   album = Album.random.first
-  album.comments.create(user: User.find(rand(3..52)),
+  album.comments.create(user: User.find(rand(3..51)),
                         body: Faker::Hipster.paragraph[0..280])
 end
