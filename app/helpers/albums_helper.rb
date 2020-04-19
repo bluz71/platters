0f1cce746834e4f94939c061c081e5b0703e2f30
Dennis B <bluz71@users.noreply.github.com>
2020-04-19 16:30:35 +1000

    Album filter fixes

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -4,8 +4,4 @@ module AlbumsHelper
   def genre_select(album)
     album.genre.present? ? album.genre.id : Genre.find_by(name: "Rock").id
   end
-
-  def filter_visibility
-    "hidden" unless params.key?("filter") && params["filter"] == "true"
-  end
 end
