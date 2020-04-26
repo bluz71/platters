e24fb9acf0b3b173896e5a0581f5f465a9ecf884
Dennis B <bluz71@users.noreply.github.com>
2020-04-26 14:43:15 +1000

    Interim bootstrap conversion for new Artist form

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable field_with_errors wrapper, see:
+#   https://coderwall.com/p/s-zwrg/remove-rails-field_with_errors-wrapper
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
+  html_tag.html_safe
+end
+
+# XXX See: https://stackoverflow.com/questions/5267998/rails-3-field-with-errors-wrapper-changes-the-page-appearance-how-to-avoid-t
+# Fix toast as well
