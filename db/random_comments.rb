77e727daa274ed77f3c02c377bf597aa3cdb60f0
Dennis B <bluz71@users.noreply.github.com>
2020-07-11 14:56:50 +1000

    Minor change

--- db/random_comments.rb
+++ db/random_comments.rb
@@ -1,5 +1,4 @@
 20.times do
   album = Album.random.first
-  album.comments.create(user: User.find(rand(3..51)),
-                        body: Faker::Hipster.paragraph[0..280])
+  album.comments.create(user: User.find(rand(3..51)), body: Faker::Hipster.paragraph[0..280])
 end
