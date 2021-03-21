62f2856a6be881a9a5b5573089644f66f621f3cc
Dennis B <bluz71@users.noreply.github.com>
2021-03-21 18:09:33 +1100

    Quiet mina deploy Bundler 2.X deprecation warnings
    Try again.

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,14 +47,12 @@ set :shared_dirs, fetch(:shared_dirs, []).push("public/packs")
 
 set :chruby_path, "/home/linuxbrew/.linuxbrew/share/chruby/chruby.sh"
 
-namespace :bundle do
-  desc "Sets the Bundler config options."
-  task :config do
-    comment %(Setting the Bundler config options)
-    command %(#{fetch(:bundle_bin)} config set --local deployment 'true')
-    command %(#{fetch(:bundle_bin)} config set --local path '#{fetch(:bundle_path)}')
-    command %(#{fetch(:bundle_bin)} config set --local without '#{fetch(:bundle_withouts)}')
-  end
+set :bundle_options, -> { "" }
+
+task :setup do
+  command "#{fetch(:bundle_bin)} config set deployment 'true'"
+  command "#{fetch(:bundle_bin)} config set path '#{fetch(:bundle_path)}'"
+  command "#{fetch(:bundle_bin)} config set without '#{fetch(:bundle_withouts)}'"
 end
 
 task :remote_environment do
